127fd62ef44e4b99de9f0af26c2cd0e156e42861
Peter DeWeese <peter.deweese@gmail.com>
2013-07-22 22:52:37 -0400

    Added responsive column.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,5 @@ DeweeSe::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.sass.cache = false
 end
