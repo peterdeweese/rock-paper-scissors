2b389e8796d076b26453c9772412a56a5cfefd8c
Peter DeWeese <peter.deweese@gmail.com>
2013-07-22 21:11:55 -0400

    Added rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeweeSe::Application.config.secret_key_base = '6552e912d6f9cd703fc88a194398d6e2ddb8f7b1ae60e6b4cb8517ff6e9a6d4549007c12ae9a259c429bc0ffb4de7b3fea7b2cb2e946e1939d186c7c40695770'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeweeSe::Application.config.session_store :cookie_store, key: '_dewee_se_session'
