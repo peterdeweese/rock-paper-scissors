95a75ed32138b8ab75cd6e915c2f4d3b0066b683
Peter DeWeese <peter.deweese@gmail.com>
2013-08-11 16:49:10 -0400

    Added win-lose-tie logic.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,20 @@ class WelcomeController < ApplicationController
   def index
   end
   
+  Beats = {Rock: :Scissors, Scissors: :Paper, Paper: :Rock}
+  Options = Beats.keys
+  
   def throw
-    flash[:notice] = 'Submitted'
-    render :action => "index"
+    render action: "index"
+    enemy_choice = Options.sample
+    user_choice = params[:user_choice].to_sym
+    
+    if enemy_choice == user_choice
+      flash[:notice] = 'You tied! Play again!' 
+    elsif Beats[user_choice] == enemy_choice
+      flash[:notice] = "#{user_choice} beats #{enemy_choice}. You win! Play again!"
+    else
+      flash[:notice] = "#{enemy_choice} beats #{user_choice}. You lose! Play again!"
+    end
   end
 end
